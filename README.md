9d8f937b1c6b90f253d2dbcf204d857afdcb2ac9
Idelfonso Gutierrez Jr <idelfonsog2@gmail.com>
2017-06-08 14:58:56 -0400

    I am Root.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Virtual Tourist
+
+This app allows users specify travel locations around the world, and create virtual photo albums for each location. The locations and photo albums will be stored in Core Data.
+
+## Software Needed
+
+- Xcode 8.3
+- Swift 3.0
+
+## Installation
+
+1. `git clone `
+2. `cd ios-virutal-tourist`
+3. `open ios-virtual-tourist.xcodeproj`
+
+## License
+MIT License
---
+++ ios-virtual-tourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-virtual-tourist
+//
+//  Created by Idelfonso Gutierrez Jr. on 6/8/17.
+//  Copyright © 2017 Idelfonso Gutierrez Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
